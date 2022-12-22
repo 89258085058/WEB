e3125e310a513f1bb0c7a81e4a2b2aa9a4f20150
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2022-12-22 12:41:12 +0300

    wqw

--- start_ui.sh
+++ start_ui.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/VALIDATION/testAuthValidation.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/VALIDATION/testDirectionsValidation.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/VALIDATION/testSettingsValidation.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/VALIDATION/testUsersKeysValidation.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/VALIDATION/testZonePathValidation.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testAuthUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testDirectionsUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testNavigationsUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testSettingsUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testStatusUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testTooltipsUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testUpdate.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testUsersKeysUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testZonePathUI.py
+
 
 
