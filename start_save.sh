b69f693f4d98990b6bd8125703dabb48ba0ce342
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2022-12-29 14:42:46 +0300

    Прогон тестов 28.12.22

--- start_save.sh
+++ start_save.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_1 tests/SAVE/testDirectionsSave.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_1 tests/SAVE/testSettingsSave.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_1 tests/SAVE/testUsersKeysSave.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_1 tests/SAVE/testZonePathSave.py
-
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_2 tests/SAVE/testDirectionsSave.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_2 tests/SAVE/testSettingsSave.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_2 tests/SAVE/testUsersKeysSave.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_2 tests/SAVE/testZonePathSave.py
 
 
--- start_ui.sh
+++ start_ui.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testAuthUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testDirectionsUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testNavigationsUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testSettingsUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testStatusUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testTooltipsUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testUpdate.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testUsersKeysUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testZonePathUI.py
-
-
-
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testAuthUI.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testNavigationsUI.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testSettingsUI.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testTooltipsUI.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testUsersKeysUI.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testZonePathUI.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testStatusUI.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testUpdate.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testDirectionsUI.py -k TestUIDestinationMainChanel
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testDirectionsUI.py -k TestUIDestinationRezerv1Chanel
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_ui_2/ tests/UI/testDirectionsUI.py -k TestUIDestinationRezerv2Chanel
