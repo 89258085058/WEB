2670724f32d884a09df9eb80b30cfc4c313fcb78
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2022-12-29 07:35:54 +0300

    t4t34

--- start_ui.sh
+++ start_ui.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testAuthUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testDirectionsUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testNavigationsUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testSettingsUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testStatusUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testTooltipsUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testUpdate.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testUsersKeysUI.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui tests/UI/testZonePathUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testAuthUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testDirectionsUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testNavigationsUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testSettingsUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testStatusUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testTooltipsUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testUpdate.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testUsersKeysUI.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_ui_1 tests/UI/testZonePathUI.py
 
 
 
--- start_validation.sh
+++ start_validation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_validation tests/VALIDATION/testAuthValidation.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_validation tests/VALIDATION/testDirectionsValidation.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_validation tests/VALIDATION/testSettingsValidation.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_validation tests/VALIDATION/testUsersKeysValidation.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_validation tests/VALIDATION/testZonePathValidation.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_validation_1 tests/VALIDATION/testAuthValidation.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_validation_1 tests/VALIDATION/testDirectionsValidation.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_validation_1 tests/VALIDATION/testSettingsValidation.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_validation_1 tests/VALIDATION/testUsersKeysValidation.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_validation_1 tests/VALIDATION/testZonePathValidation.py
 
 
