12a020d5b630dde364ff487ced34e093912a41ec
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2022-12-30 14:31:51 +0300

    ропрг

--- start_save.sh
+++ start_save.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_2 tests/SAVE/testDirectionsSave.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_2 tests/SAVE/testSettingsSave.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_2 tests/SAVE/testUsersKeysSave.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_2 tests/SAVE/testZonePathSave.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_3 tests/SAVE/testDirectionsSave.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_3 tests/SAVE/testSettingsSave.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_3 tests/SAVE/testUsersKeysSave.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_3 tests/SAVE/testZonePathSave.py
 
 
