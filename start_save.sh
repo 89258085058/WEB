26c690bb9d2fc6613e1fb85cc7c28421bebad897
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2023-01-16 07:13:52 +0300

    gwegewg

--- start_save.sh
+++ start_save.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_3 tests/SAVE/testDirectionsSave.py
 python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_3 tests/SAVE/testSettingsSave.py
 python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_3 tests/SAVE/testUsersKeysSave.py
 python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_3 tests/SAVE/testZonePathSave.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_3 tests/SAVE/testDirectionsSave.py -k TestSaveDestinationTumblers
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_save_3 tests/SAVE/testDirectionsSave.py -k TestSaveDestinationChanels
 
 
