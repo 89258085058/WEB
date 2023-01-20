d390f50f9e7804cbc20aa101912854ec2b718de8
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2023-01-20 13:36:35 +0300

    ewgewg

--- Zone_path.sh
+++ Zone_path.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_zone_path tests/SAVE/testZonePathSave.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_zone_path tests/SAVE/testZonePathSave.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_validation_6 tests/VALIDATION/testZonePathValidation.py -k test_path_input_search
+#python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_zone_path tests/SAVE/testZonePathSave.py
 
 
 
