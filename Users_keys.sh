1eadb7c65799b82347f3dde65cebc57a6938da2d
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2023-01-23 14:11:09 +0300

    test

--- Users_keys.sh
+++ Users_keys.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_users tests/VALIDATION/testUsersKeysValidation.py -k test_input_keys_id_settings
-#python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_users tests/SAVE/testUsersKeysSave.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_users tests/VALIDATION/testUsersKeysValidation.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_users tests/SAVE/testUsersKeysSave.py
