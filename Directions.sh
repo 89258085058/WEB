d0cf32a272743470cbb1d2d247a1a3333d459463
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2023-01-30 09:59:22 +0300

    rhrehre

--- Directions.sh
+++ Directions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_directions_01 tests/VALIDATION/testDirectionsValidation.py -k TestValidationDestinationMainChanel
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_directions_01 tests/VALIDATION/testDirectionsValidation.py -k TestValidationDestinationRezerv1Chanel
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_directions_01 tests/VALIDATION/testDirectionsValidation.py -k TestValidationDestinationRezerv2Chanel
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_directions_01 tests/SAVE/testDirectionsSave.py -k TestSaveDestinationTumblers
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_directions_01 tests/SAVE/testDirectionsSave.py -k TestSaveDestinationChanels
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/VALIDATION/testDirectionsValidation.py -k TestValidationDestinationMainChanel
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/VALIDATION/testDirectionsValidation.py -k TestValidationDestinationRezerv1Chanel
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/VALIDATION/testDirectionsValidation.py -k TestValidationDestinationRezerv2Chanel
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/SAVE/testDirectionsSave.py -k TestSaveDestinationTumblers
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/SAVE/testDirectionsSave.py -k TestSaveDestinationChanels
