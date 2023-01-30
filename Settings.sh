2da91c1d2b482de7f182e14de67f45e6aba8e0b0
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2023-01-30 12:55:26 +0300

    gregre

--- Settings.sh
+++ Settings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_settings_01 tests/VALIDATION/testSettingsValidation.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_settings_01 tests/SAVE/testSettingsSave.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_settings_02 tests/VALIDATION/testSettingsValidation.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_settings_02 tests/SAVE/testSettingsSave.py
