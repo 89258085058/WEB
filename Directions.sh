1ad4a0bc56883264b20e3b9ab5b91e8d3ccecfc2
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2023-01-30 10:23:58 +0300

    екеркерке

--- Directions.sh
+++ Directions.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/VALIDATION/testDirectionsValidation.py -k TestValidationDestinationMainChanel
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/VALIDATION/testDirectionsValidation.py -k TestValidationDestinationRezerv1Chanel
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/VALIDATION/testDirectionsValidation.py -k TestValidationDestinationRezerv2Chanel
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/VALIDATION/testDirectionsValidation.py
 python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/SAVE/testDirectionsSave.py -k TestSaveDestinationTumblers
 python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_directions_02 tests/SAVE/testDirectionsSave.py -k TestSaveDestinationChanels
