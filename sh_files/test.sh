d7c56bea5ebd712526c0d608311028bf16c24163
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2023-02-01 12:59:31 +0300

    thrthh

--- sh_files/test.sh
+++ sh_files/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_settings_02 tests/VALIDATION/testSettingsValidation.py -k test_input_data_name_object
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_test tests/VALIDATION/testSettingsValidation.py -k test_input_data_name_object
