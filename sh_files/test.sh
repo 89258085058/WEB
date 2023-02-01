920ed8c763bd158d2cf2ee270f7a568ed32ae649
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2023-02-01 13:14:42 +0300

    egewgew

--- sh_files/test.sh
+++ sh_files/test.sh
@@ -2,5 +2,4 @@
 
 
 python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_test tests/VALIDATION/testSettingsValidation.py -k test_input_data_name_object
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_test tests/VALIDATION/testSettingsValidation.py -k test_input_data_name_object_negativ
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_test tests/VALIDATION/testSettingsValidation.py -k test_input_data_number_object
+
