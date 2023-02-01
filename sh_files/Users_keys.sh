6d90d6bf9c9beecc3a540ddb58b01e573f63c4ad
BolidTest <121028860+BolidTest@users.noreply.github.com>
2023-02-01 19:05:22 +0300

    Update Users_keys.sh

--- sh_files/Users_keys.sh
+++ sh_files/Users_keys.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_users_02 tests/VALIDATION/testUsersKeysValidation.py
-python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_users_02 tests/SAVE/testUsersKeysSave.py
+
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_users_02 tests/VALIDATION/testUsersKeysValidation.py -k test_user_input_name_user
+#python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_users_02 tests/SAVE/testUsersKeysSave.py
