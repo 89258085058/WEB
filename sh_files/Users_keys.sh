b070ebcac3dfed6ceab92e2d54cafca465f48106
Alexandr Gorelov <86528556+89258085058@users.noreply.github.com>
2023-02-01 15:45:36 +0300

    ewgewgew

--- sh_files/Users_keys.sh
+++ sh_files/Users_keys.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_users_02 tests/VALIDATION/testUsersKeysValidation.py -k test_user_input_search_user
-#python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_users_02 tests/SAVE/testUsersKeysSave.py
+python3 -m pytest  --device=remote --browser=selenoid --alluredir=allure_users_02 tests/VALIDATION/testUsersKeysValidation.py
+python3 -m pytest --device=remote --browser=selenoid --alluredir=allure_users_02 tests/SAVE/testUsersKeysSave.py
