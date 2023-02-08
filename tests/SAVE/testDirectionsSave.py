bfc6d2bb81556c092ff4b54248fe30a1a3a71e1a
Alexander-Santalov <vgtrk520@yandex.ru>
2023-02-08 10:44:11 +0300

    поправил testDirectionsSave

--- tests/SAVE/testDirectionsSave.py
+++ tests/SAVE/testDirectionsSave.py
@@ -10,7 +10,7 @@ reruns = 1
 
 # directions_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
 
-directions_list = [f'{random.randint(1,16)}']
+directions_list = ['1']
 
 
 @pytest.fixture()
