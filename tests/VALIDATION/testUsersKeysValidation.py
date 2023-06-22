fc2c058cb6bba2249cab4f8c41d5d1ed47330c05
Alexander-Santalov <vgtrk520@yandex.ru>
2023-06-22 10:11:40 +0300

    поправил фикстуру в testUsersKeysValidation

--- tests/VALIDATION/testUsersKeysValidation.py
+++ tests/VALIDATION/testUsersKeysValidation.py
@@ -4,7 +4,7 @@ import allure
 import pytest
 from selenium.webdriver.common.by import By
 
-reruns = 1
+reruns = 2
 
 
 @pytest.fixture
@@ -52,7 +52,7 @@ def keys_settings(app):
 @pytest.fixture()
 def close_modal(request, app):
     def fin():
-        app.method.click((By.XPATH, '(//*[.=" Отменить "]//div)[last()]'))
+        app.method.check_hide_element('modal-title', '(//*[.=" Отменить "]//div)[last()]')
 
     request.addfinalizer(fin)
 
